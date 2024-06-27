7e240de215e25ca9d34649e94bc8412fcb0a78ef
felipejordanc <felipe.jordan.c@gmail.com>
2024-06-27 16:16:43 -0400

    update to Collection 2

--- scripts/download_batch.py
+++ scripts/download_batch.py
@@ -49,22 +49,25 @@ def keepClear(region,sat):
     def keepClear_child(image):
         # Select FMASK QA band and select bits associated to clouds and clouds cover
         im  = ee.Image(image)
-        qa  = im.select('pixel_qa')
-        qa_clouds            = extractQABits(qa,6,7)
-        qa_cloudsShadows     = extractQABits(qa,3,3)
+        qa  = im.select('QA_PIXEL')
+        qa_clouds            = extractQABits(qa,8,9)
+        qa_cloudsShadows     = extractQABits(qa,10,11)
         # Select Saturation QA band and select bite associated to saturation
-        qa2 = im.select('radsat_qa')
-        qa_saturation        = extractQABits(qa2,0,0)
+        qa2 = im.select('QA_RADSAT')
+        if sat=='L8':
+            qa_saturation        = extractQABits(qa2,1,3)
+        else:
+            qa_saturation        = extractQABits(qa2,0,2)
         # Create mask where valid pixels have low confidence of clound and clound shadow and are not saturated
-        mask                 = qa_clouds.lte(1).And(qa_cloudsShadows.eq(0)).And(qa_saturation.eq(0))
+        mask                 = qa_clouds.lte(1).And(qa_cloudsShadows.lte(1)).And(qa_saturation.eq(0))
         if sat=='L8':
             # Cirrus clounds and terrain oclussion in landsat 8
-            qa_cirrus  = extractQABits(qa,8,9)
-            qa_terrain = extractQABits(qa,10,10)
+            qa_cirrus  = extractQABits(qa,14,15)
+            qa_terrain = extractQABits(qa2,11,11)
             mask       = mask.And(qa_cirrus.lte(1)).And(qa_terrain.eq(0))
         # Claculate fraction of valid pixels in the region and return image with QI property with fraction of valid pixels
-        valid = mask.reduceRegion(ee.Reducer.sum(),region).get('pixel_qa')
-        tot   = mask.reduceRegion(ee.Reducer.count(),region).get('pixel_qa')
+        valid = mask.reduceRegion(ee.Reducer.sum(),region).get('QA_PIXEL')
+        tot   = mask.reduceRegion(ee.Reducer.count(),region).get('QA_PIXEL')
         return im.updateMask(mask).copyProperties(im).set({'QI':ee.Number(valid).divide(tot)})
     return keepClear_child
 
@@ -148,19 +151,19 @@ def makeName(s):
 # Main class that create image from sensor
 class downloadImagery():
     def __init__(self,folder,year,sensor,size,topocorrection=True):
-        collections = {'L5':{'SR':['LANDSAT/LT05/C01/T1_SR','LANDSAT/LT05/C01/T2_SR'],'TOA':['LANDSAT/LT05/C01/T1_TOA','LANDSAT/LT05/C01/T2_TOA']},
-                       'L7':{'SR':['LANDSAT/LE07/C01/T1_SR','LANDSAT/LE07/C01/T2_SR'],'TOA':['LANDSAT/LE07/C01/T1_TOA','LANDSAT/LE07/C01/T2_TOA']},
-                       'L8':{'SR':['LANDSAT/LC08/C01/T1_SR','LANDSAT/LC08/C01/T2_SR'],'TOA':['LANDSAT/LC08/C01/T1_TOA','LANDSAT/LC08/C01/T2_TOA']},
+        collections = {'L5':{'SR':['LANDSAT/LT05/C02/T1_SR','LANDSAT/LT05/C02/T2_SR'],'TOA':['LANDSAT/LT05/C02/T1_TOA','LANDSAT/LT05/C02/T2_TOA']},
+                       'L7':{'SR':['LANDSAT/LE07/C02/T1_SR','LANDSAT/LE07/C02/T2_SR'],'TOA':['LANDSAT/LE07/C02/T1_TOA','LANDSAT/LE07/C02/T2_TOA']},
+                       'L8':{'SR':['LANDSAT/LC08/C02/T1_SR','LANDSAT/LC08/C02/T2_SR'],'TOA':['LANDSAT/LC08/C02/T1_TOA','LANDSAT/LC08/C02/T2_TOA']},
                        }
-        final_bands        = {'L5':[['B1','B2','B3','B4','B5','B6','B7'],
+        final_bands        = {'L5':[['SR_B1','SR_B2','SR_B3','SR_B4'],
                                     [],
-                                    ['B4','B3']],
-                              'L7':[['B1','B2','B3','B4','B5','B6','B7'],
+                                    ['SR_B4','SR_B3']],
+                              'L7':[['SR_B1','SR_B2','SR_B3','SR_B4'],
                                     ['B8'],
-                                    ['B4','B3']],
-                              'L8':[['B1','B2','B3','B4','B5','B6','B7','B10','B11'],
+                                    ['SR_B4','SR_B3']],
+                              'L8':[['SR_B2','SR_B3','SR_B4','SR_B5'],
                                     ['B8'],
-                                    ['B5','B4']]}
+                                    ['SR_B5','SR_B4']]}
         self.folder  = folder
         self.year    = year
         self.sensor  = sensor
